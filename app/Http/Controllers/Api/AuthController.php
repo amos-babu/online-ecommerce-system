ecb73ab01f72fabf92c133ba8e0929f18fe4f586
Amos Babu <okumuamos88@gmail.com>
2024-06-10 09:35:59 +0300

    "Commit 4"

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class AuthController extends Controller
 {
@@ -16,15 +17,21 @@ class AuthController extends Controller
         $validator = Validator::make($request->all(), [
             'name'      => 'required|string|max:255',
             'email'     => 'required|string|max:255|unique:users',
-            'password'  => 'required|string'
+            'username'  => 'required|string|max:255|unique:users',
+            'password'  => 'required|string|min:8'
         ]);
 
+
         if ($validator->fails()) {
-            return response()->json($validator->errors());
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $validator->errors()
+            ], 422);
         }
 
         $user = User::create([
             'name'  => $request->name,
+            'username'  => $request->username,
             'email'  => $request->email,
             'password'  => Hash::make($request->password)
         ]);
@@ -41,16 +48,20 @@ class AuthController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'email' => 'required|string|max:255',
-            'password' => 'required|string'
+            'password' => 'required|string|min:8'
         ]);
+
         if ($validator->fails()) {
-            return response()->json($validator->errors());
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $validator->errors()
+            ], 422);
         }
 
         $credentials = $request->only('email', 'password');
         if (!Auth::attempt($credentials)) {
             return response()->json([
-                'message' => 'User Not Found'
+                'message' => 'Authentication Failed!'
             ], 401);
         }
 
@@ -63,6 +74,63 @@ class AuthController extends Controller
             'token_type' => 'Bearer'
         ]);
     }
+
+    public function updateuser(Request $request, User $user)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'sometimes|string|max:255',
+            'username' => 'sometimes|string|max:255|unique:users,username,' . $user->id,
+            'email' => 'sometimes|string|email|max:255|unique:users,email,' . $user->id,
+            'profile_image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'password'  => 'sometimes|string|min:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        if ($request->has('name')) {
+            $user->name = $request->name;
+        }
+
+        if ($request->has('username')) {
+            $user->username = $request->username;
+        }
+
+        if ($request->has('email')) {
+            $user->email = $request->email;
+        }
+
+        if ($request->has('password')) {
+            $user->password = $request->password;
+        }
+
+        // $user->name = $request->input('name');
+        // $user->username = $request->input('username');
+        // $user->email = $request->input('email');
+        // $user->password = $request->input('password');
+
+        if ($request->hasFile('profile_image')) {
+            if ($user->profile_image) {
+                Storage::delete('public/profile_images/' . $user->profile_image);
+            }
+
+            $imagePath = $request->file('profile_image')->store('profile_images', 'public');
+            $user->profile_image = basename($imagePath);
+        }
+
+        $user->save();
+
+        return response()->json([
+            'message' => 'User profile updated.',
+            'user' => $user
+        ], 200);
+    }
+
 
     public function logout(User $user)
     {
--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,5 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::put('/update/{username}', [AuthController::class, 'updateuser']);
 });
